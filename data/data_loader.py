0f2c0a6577a550e03b425801af05bc3f743187ba
brandonviaje <brandon.viaje@ontariotechu.net>
2025-04-29 00:34:16 -0400

    implement dataloader

--- data/data_loader.py
+++ data/data_loader.py
@@ -2,11 +2,12 @@ import torch
 import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
 # Load CIFAR10 Dataset
 cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=False, transform=transforms.ToTensor())
 cifar_testset = datasets.CIFAR10(root='./data', train=False, download=False, transform=transforms.ToTensor())
 
 # DataLoader 
-train_loader = torch.utils.data.DataLoader(cifar_trainset, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(cifar_testset, batch_size=64, shuffle=False)
+train_loader = DataLoader(cifar_trainset, batch_size=64, shuffle=True)
+test_loader = DataLoader(cifar_testset, batch_size=64, shuffle=False)
